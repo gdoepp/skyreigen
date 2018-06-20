4cb0697f83831c318113103a4355bec5ea020307
gd <gd>
2018-06-20 22:45:03 +0200

    clean up

--- jni/aaplus_wrap.cpp
+++ jni/aaplus_wrap.cpp
@@ -230,7 +230,7 @@ static void SWIGUNUSED SWIG_JavaThrowException(JNIEnv *jenv, SWIG_JavaExceptionC
 #define SWIG_contract_assert(nullreturn, expr, msg) if (!(expr)) {SWIG_JavaThrowException(jenv, SWIG_JavaIllegalArgumentException, msg); return nullreturn; } else
 
 
-#include <../AA+.h>
+#include <./AA+.h>
 
 
 #ifdef __cplusplus
--- jni/astro_wrap.cpp
+++ jni/astro_wrap.cpp
@@ -230,7 +230,7 @@ static void SWIGUNUSED SWIG_JavaThrowException(JNIEnv *jenv, SWIG_JavaExceptionC
 #define SWIG_contract_assert(nullreturn, expr, msg) if (!(expr)) {SWIG_JavaThrowException(jenv, SWIG_JavaIllegalArgumentException, msg); return nullreturn; } else
 
 
-#include <../VisLimit.h>
+#include <./VisLimit.h>
 
 
 #ifdef __cplusplus
