5fb29a74e2cb033e2a6c918ebe8265dcd65ead1f
gd <gd>
2020-05-24 11:53:49 +0200

    update to AA+ 2.14

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,2 +1,5 @@
 APP_ABI := armeabi-v7a,arm64-v8a
 APP_STL := c++_shared
+
+APP_PLATFORM := android-27
+APP_BUILD_SCRIPT := Android.mk
--- jni/build.sh
+++ jni/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 : "${NDK_HOME:=/opt/android/ndk}"
-
+echo creating interfaces to java:
 cd swiggen
 ./swiggen.sh
+echo now compiling c++ with $NDK_HOME:...
 cd ..
 $NDK_HOME/ndk-build
+echo copying libraries...
 cp -r ../libs/* ../src/main/jniLibs/
+echo ready
